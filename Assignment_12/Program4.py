8494edbab0a23058d43a1da9c75919cf0f5dc952
ankitajannu <ankitajannu11@gmail.com>
2026-01-30 21:22:29 +0530

    Update Program4.py

--- Assignment_12/Program4.py
+++ Assignment_12/Program4.py
@@ -1,4 +1,4 @@
-def Factors(No):
+def Display(No):
     for i in range(1, No+1):
         print(i)
         
@@ -8,7 +8,8 @@ def main():
     print("Enter number : ")
     Value = int(input())
 
-    Factors(Value)
+    Display(Value)
 
 if __name__ == "__main__":
+
     main()
